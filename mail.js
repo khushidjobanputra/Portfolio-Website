60878f9422b34185c45525a975314fcdfcf8aabf
khushi3114 <khushidjobanputra77@gmail.com>
2023-04-09 19:02:29 +0530

    initialized new repo

---
+++ mail.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBH_YpB6wY5dlcQshFYZQ-olRStdWme_u0",
+    authDomain: "contact-form-5ffe2.firebaseapp.com",
+    databaseURL: "https://contact-form-5ffe2-default-rtdb.firebaseio.com",
+    projectId: "contact-form-5ffe2",
+    storageBucket: "contact-form-5ffe2.appspot.com",
+    messagingSenderId: "766803268761",
+    appId: "1:766803268761:web:33c4359dd20bd477cb1408"
+  };
